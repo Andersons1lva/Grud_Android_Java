dd8f45002198ab7d753a9f44c01b421d23d0ec6d
andersons1lva <andersons1lva@yahoo.com.br>
2022-08-28 01:17:38 -0300

    Settings Toolbar and access to control inside gives MainActivity.kt

--- app/src/main/java/com/anderson/mysubscribers/MainActivity.kt
+++ app/src/main/java/com/anderson/mysubscribers/MainActivity.kt
@@ -2,10 +2,24 @@ package com.anderson.mysubscribers
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import com.anderson.mysubscribers.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //access to control denture da main activity
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        binding.appToolbar.setupWithNavController(navController, appBarConfiguration)
     }
 }
