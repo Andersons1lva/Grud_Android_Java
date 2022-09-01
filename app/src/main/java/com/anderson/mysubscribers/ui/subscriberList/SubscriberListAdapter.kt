e344f72cdcea4f940aacbd9d30f19eea1efa1124
andersons1lva <andersons1lva@yahoo.com.br>
2022-08-31 21:36:57 -0300

    Implement class SubscriberListAdapter.kt

---
+++ app/src/main/java/com/anderson/mysubscribers/ui/subscriberList/SubscriberListAdapter.kt
@@ -0,0 +1,40 @@
+package com.anderson.mysubscribers.ui.subscriberList
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.anderson.mysubscribers.data.db.entity.SubscriberEntity
+import com.anderson.mysubscribers.databinding.SubscriberItemBinding
+
+
+class SubscriberListAdapter(
+    private val subscribers: List<SubscriberEntity>,
+    private val context: Context
+) : RecyclerView.Adapter<SubscriberListAdapter.SubscriberListViewHolder>() {
+
+    // no onCreateViewHolder e para inflar o layout
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SubscriberListViewHolder {
+        val binding = SubscriberItemBinding.inflate(LayoutInflater.from(context), parent, false)
+        return SubscriberListViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: SubscriberListViewHolder, position: Int) {
+        holder.bindView(subscribers[position])
+    }
+
+    override fun getItemCount() = subscribers.size
+
+    class SubscriberListViewHolder(binding: SubscriberItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        private val textViewSubscriberName: TextView = binding.textSubscriberName
+        private val textViewSubscriberEmail: TextView = binding.textSubscriberEmail
+
+        fun bindView(subscriber: SubscriberEntity) {
+            textViewSubscriberName.text = subscriber.name
+            textViewSubscriberEmail.text = subscriber.email
+        }
+
+    }
+}
--- app/src/main/java/com/anderson/mysubscribers/ui/subscriberList/SubscriberListFragment.kt
+++ app/src/main/java/com/anderson/mysubscribers/ui/subscriberList/SubscriberListFragment.kt
@@ -1,11 +1,11 @@
 package com.anderson.mysubscribers.ui.subscriberList
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.anderson.mysubscribers.R
+import com.anderson.mysubscribers.data.db.entity.SubscriberEntity
 import com.anderson.mysubscribers.databinding.FragmentSubscriberListBinding
 
 class SubscriberListFragment : Fragment(R.layout.fragment_subscriber_list) {
@@ -13,18 +13,30 @@ class SubscriberListFragment : Fragment(R.layout.fragment_subscriber_list) {
     private var _binding: FragmentSubscriberListBinding? = null
     private val binding get() = _binding!!
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        _binding = FragmentSubscriberListBinding.inflate(inflater, container, false)
-        return binding.root
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        _binding =
+            FragmentSubscriberListBinding.bind(view)  //if the view is already inflated then we can just bind it to view binding.
+
+        val subscriberListAdapter = SubscriberListAdapter(
+            listOf(
+                SubscriberEntity(1, "Anderson", "Andersons1lva@yahoo.com.br"),
+                SubscriberEntity(2, "Bryan", "Bryan@yahoo.com.br")
+            )
+        )
+
+        binding.recyclerSubscribers.run {
+            setHasFixedSize(true)
+            adapter = subscriberListAdapter
+        }
     }
 
+    //Note: Fragments outlive their views. Make sure you clean up any references to the binding class
+    // instance in the fragment's onDestroyView() method.
     override fun onDestroyView() {
+        Toast.makeText(activity, "On destroy", Toast.LENGTH_SHORT).show()
         super.onDestroyView()
         _binding = null
     }
-
 }
